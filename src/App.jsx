f362a1a4405f18bc7183ecbda3cfb8e689bb1b3c
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-02-02 11:56:28 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,7 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import config from './amplifyconfiguration.json';
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      loginWith: {
-        oauth: {
-          redirectSignIn: [
-            "http://localhost:3000/",
-            "https://main.dzj2kg93vmtwj.amplifyapp.com/",
-          ],
-          redirectSignOut: [
-            "http://localhost:3000/",
-            "https://main.dzj2kg93vmtwj.amplifyapp.com/",
-          ],
-        },
-      },
-    },
-  },
-  ...config
-});
+Amplify.configure(config);
 
 export default function MyApp() {
   const [isSignedIn, setIsSignedIn] = React.useState(false);
